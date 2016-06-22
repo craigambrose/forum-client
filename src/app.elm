d201c67c9cd5156d5ae941fbc98d02480b1df0ef
craigambrose <craig@craigambrose.com>
2016-06-22 20:43:35 +0100

    use a model

--- src/app.elm
+++ src/app.elm
@@ -1,9 +1,36 @@
 import Html exposing (article, h1, div, p, text, img)
+import Html.App exposing (beginnerProgram)
 import Html.Attributes exposing (src, class)
 
 main =
+  beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model =
+  { title : String
+  , imageUrl : String
+  , url: String
+  , description: String
+  }
+
+model : Model
+model =
+  Model "Here is the original article title" "https://placekitten.com/200/300" "fish" "Here is a more detailed description. It is usually a couple of sentences."
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  model
+
+-- VIEW
+
+view model =
   article [] [
-    h1 [] [text "Here is the original article title"],
-    p [class "description"] [text "Here is a more detailed description. It is usually a couple of sentences."],
-    img [src "https://placekitten.com/200/300"] []
+    h1 [] [text model.title],
+    p [class "description"] [text model.description],
+    img [src model.imageUrl] []
   ]
